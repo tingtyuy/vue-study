21f82d27d454f2ebdfb2f491a56bda44aabb5093
mengkai <363255751@qq.com>
2025-01-21 17:07:58 +0800

    1

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,11 +1,11 @@
 <template>
-    <div :class="headerDirection" :style="{ backgroundColor :carbackgroundColor}">
-        <el-text :class="numberPosition" size="large"> 0{{ number }}</el-text>
+    <div :class="headerDirection" :style="{ backgroundColor: carbackgroundColor }">
+        <el-text :class="numberPosition" size="large"> {{ cnumber }}</el-text>
 
     </div>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref,computed } from 'vue';
 const props = defineProps({
     headerDirection:
     {
@@ -30,6 +30,9 @@ const props = defineProps({
         type: Boolean,
         default: false
     }
+})
+const cnumber = computed(() => {
+  return props.number >100 ? props.number : '0'+rops.number
 })
 let carbackgroundColor = ref("#909090");
 function setCarColor() {
@@ -57,33 +60,64 @@ init()
 
 .vertical {
     height: 3.5rem;
-    width: 1.1rem;
+    width: 1.3rem;
     // background-color: gray;
     z-index: 3;
     /* border: 1px solid green; */
+    // margin-top: 0.2rem;
     margin-top: 0.2rem;
     margin-bottom: 0.2rem;
+    margin-right: 0.2rem;
     position: relative;
 }
 
 .left {
-    font-size: 0.7rem;
+    font-size: 0.5rem;
     color: rgb(218, 218, 218);
     margin-left: 0.5rem;
-
+    position: absolute;
+    // line-height: 1.2rem;
+    top: 0.3rem;
 }
 
 .right {
-    font-size: 0.7rem;
+    font-size: 0.5rem;
     color: rgb(218, 218, 218);
     position: absolute;
     right: 0.5rem;
+    top: 0.3rem;
 }
 
 .center {
-    font-size: 0.7rem;
+    font-size: 0.5rem;
     color: rgb(218, 218, 218);
     position: absolute;
     right: 1rem;
+    top: 0.3rem;
+}
+
+.rleft {
+    font-size: 0.5rem;
+    color: rgb(218, 218, 218);
+    margin-left: 0.5rem;
+    position: absolute;
+    // line-height: 1.2rem;
+    top: 0.3rem;
+}
+
+.rright {
+    font-size: 0.5rem;
+    color: rgb(218, 218, 218);
+    position: absolute;
+    right: 0.25rem;
+    bottom: 0.5rem;
+}
+
+.rcenter {
+    font-size: 0.5rem;
+    color: rgb(218, 218, 218);
+    position: absolute;
+    left: 0.2rem;
+    top: 1.2rem;
 }
 </style>
--- src/components/CarPark.vue
+++ src/components/CarPark.vue
@@ -1,29 +1,82 @@
 <template>
   <div class="parking-lot-container" ref="container">
     <el-text id="label" size="large">洗车台</el-text>
-
+    <el-text id="labelleft" size="large">膜结构车棚</el-text>
+    <el-text id="labelright" size="large">地下车库</el-text>
+    <!-- left -->
     <div id="carparkleft">
-      <Car :number="n" v-for=" n in 19" :key="n" number-position="right" :has-car="true"></Car>
+      <Car :number="n.number" v-for=" n in l1" :key="n" number-position="right" :has-car="n.isStop"></Car>
     </div>
     <div class="overlay"></div>
-
     <div id="carparkcenter">
-      <Car :number="n" v-for=" n in 18  " :key="n"></Car>
+      <Car :number="n.number" v-for=" n in l2" :key="n" number-position="left" :has-car="n.isStop"></Car>
     </div>
     <div id="carparkcenter2">
-      <Car :number="n" v-for=" n in 20" :key="n" number-position="right"></Car>>
+      <Car :number="n.number" v-for=" n in l3" :key="n" number-position="right" :has-car="n.isStop"></Car>
     </div>
-
-
     <div id="carparkright">
-      <Car :number="n" v-for=" n in 18  " :key="n"></Car>
+      <Car :number="n.number" v-for=" n in l4" :key="n" number-position="left" :has-car="n.isStop"></Car>
     </div>
     <div id="carparkright2">
-      <Car :number="n" v-for=" n in 20" :key="n" number-position="right"></Car>>
+      <Car :number="n.number" v-for=" n in l5" :key="n" number-position="right" :has-car="n.isStop"></Car>
+    </div>
+    <!-- right -->
+    <div id="r1">
+      <Car :number="n.number" v-for=" n in r1" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rcenter"></Car>
     </div>
 
-    <div id="rightleft">
-      <Car :number="n" v-for=" n in 19" :key="n" number-position="right" :has-car="true"></Car>
+    <div id="r2">
+      <Car :number="n.number" v-for=" n in r2" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r3">
+      <Car :number="n.number" v-for=" n in r3" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r4">
+      <Car :number="n.number" v-for=" n in r4" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r5">
+      <Car :number="n.number" v-for=" n in r5" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r6">
+      <Car :number="n.number" v-for=" n in r6" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r7">
+      <Car :number="n.number" v-for=" n in r7" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r8">
+      <Car :number="n.number" v-for=" n in r8" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r9">
+      <Car :number="n.number" v-for=" n in r9" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+
+    <div id="r10">
+      <Car :number="n.number" v-for=" n in r10" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+    <div id="r11">
+      <Car :number="n.number" v-for=" n in r11" :key="n" number-position="right" headerDirection="vertical"
+        :has-car="n.isStop" numberPosition="rright"></Car>
+    </div>
+    <div id="r12">
+      <Car :number="n.number" v-for=" n in r12" :key="n" number-position="right" headerDirection="horizontal"
+        :has-car="n.isStop" numberPosition="right"></Car>
     </div>
 
   </div>
@@ -31,29 +84,83 @@
 
 <script setup>
 import Car from '../components/Car.vue';
+import { ref } from 'vue';
 
-let data = [
-  { number: 1, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
-  { number: 2, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
-  { number: 3, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
-  { number: 4, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
-  { number: 5, isStop: false, isLeave: false, isEmpty: true ,place:"地下车库"},
-  { number: 6, isStop: false, isLeave: false, isEmpty: true ,place:"地下车库"}
-]
-let lLeft=ref([]);
-let lCenter=ref([]);
-let lRight=ref([]);
 
-function allotData()
-{
-  lLeft.value=data.number.filter()
+const startNumber = 1;
+const endNumber = 95;
+
+const ldata = Array.from({ length: endNumber - startNumber + 1 }, (_, index) => ({
+  number: startNumber + index,
+  isStop: false,
+  isLeave: false,
+  isEmpty: true
+}));
+const rstartNumber = 1;
+const rendNumber = 191;
+const rdata = Array.from({ length: rendNumber - rstartNumber + 1 }, (_, index) => ({
+  number: rstartNumber + index,
+  isStop: false,
+  isLeave: false,
+  isEmpty: true
+}));
+
+let l1 = ref([]);
+let l2 = ref([]);
+let l3 = ref([]);
+let l4 = ref([]);
+let l5 = ref([]);
+
+let r1 = ref([]);
+let r2 = ref([]);
+let r3 = ref([]);
+let r4 = ref([]);
+let r5 = ref([]);
+let r6 = ref([]);
+let r7 = ref([]);
+let r8 = ref([]);
+let r9 = ref([]);
+let r10 = ref([]);
+let r11 = ref([]);
+let r12 = ref([]);
+
+function allotData() {
+  // left
+  l1.value = ldata.filter(item => item.number <= 19).reverse();
+  l2.value = ldata.filter(item => item.number <= 37 && item.number >= 20);
+  l3.value = ldata.filter(item => item.number <= 57 && item.number >= 38).reverse();
+  l4.value = ldata.filter(item => item.number <= 75 && item.number >= 58);
+  l5.value = ldata.filter(item => item.number <= 95 && item.number >= 76).reverse();
+
+  // right
+  r1.value = rdata.filter(item => item.number >= 152 && item.number <= 154).reverse();
+  r2.value = rdata.filter(item => item.number >= 25 && item.number <= 36);
+  r3.value = rdata.filter(item => item.number == 88);
+  r4.value = rdata.filter(item => item.number >= 89 && item.number <= 98);
+
+
+  r5.value = rdata.filter(item => item.number >= 13 && item.number <= 24);
+  r6.value = rdata.filter(item => item.number == 77);
+  r7.value = rdata.filter(item => item.number >= 78 && item.number <= 87);
+
+
+  r8.value = rdata.filter(item => item.number >= 1 && item.number <= 12);
+  r9.value = rdata.filter(item => item.number == 66);
+  r10.value = rdata.filter(item => item.number >= 67 && item.number <= 76);
+
+  r11.value = rdata.filter(item => item.number >= 125 && item.number <= 151).reverse();
+  r12.value = rdata.filter(item => item.number >= 187 && item.number <= 191);
 
 }
-function init(){
- 
 
+function makeData() {
+  l1.value.filter(item => item.number < 9).forEach(item => item.isStop = true);
 }
- init();
+function init() {
+  allotData();
+  makeData();
+}
+init();
 </script>
 
 <style lang="scss" scoped>
@@ -66,6 +173,25 @@ $parking-lot: '../assets/parking-lot.jpg';
   color: rgb(7, 88, 88);
   left: 21.5rem;
   top: 0.93rem;
+  position: absolute;
+  z-index: 2;
+}
+
+
+#labelleft {
+  font-size: 1.2rem;
+  color: rgb(7, 88, 88);
+  left: 15rem;
+  bottom: 0.01rem;
+  position: absolute;
+  z-index: 2;
+}
+
+#labelright {
+  font-size: 1.2rem;
+  color: rgb(7, 88, 88);
+  left: 65rem;
+  bottom: 0.01rem;
   position: absolute;
   z-index: 2;
 }
@@ -113,6 +239,130 @@ $parking-lot: '../assets/parking-lot.jpg';
 #carparkleft {
   left: 1.2rem;
   top: 4rem;
+  position: absolute;
+  height: 30rem;
+  width: 4rem;
+  // border: 1px solid red;
+  z-index: 2;
+}
+
+#r1 {
+  left: 32.2rem;
+  top: 10rem;
+  position: absolute;
+  height: 30rem;
+  width: 4rem;
+  // border: 1px solid red;
+  z-index: 2;
+}
+
+
+#r2 {
+  left: 36.5rem;
+  top: 7.6rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 20rem;
+  z-index: 2;
+}
+
+.vertical:nth-child(3n) {
+  margin-right: 0.5rem;
+}
+
+#r3 {
+  left: 59.7rem;
+  top: 7.6rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 2rem;
+  z-index: 2;
+}
+
+#r4 {
+  left: 61.5rem;
+  top: 7.6rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 20rem;
+  z-index: 2;
+}
+
+#r5 {
+  left: 36.5rem;
+  top: 17.4rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 20rem;
+  z-index: 2;
+}
+
+#r6 {
+  left: 59.7rem;
+  top: 17.4rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 2rem;
+  z-index: 2;
+}
+
+#r7 {
+  left: 61.5rem;
+  top: 17.4rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 20rem;
+  z-index: 2;
+}
+
+#r8 {
+  left: 36.5rem;
+  top: 21.3rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 20rem;
+  z-index: 2;
+}
+
+#r9 {
+  left: 59.7rem;
+  top: 21.3rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 2rem;
+  z-index: 2;
+}
+
+#r10 {
+  left: 61.5rem;
+  top: 21.3rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 20rem;
+  z-index: 2;
+}
+
+#r11 {
+  left: 34.8rem;
+  top: 31.3rem;
+  position: absolute;
+  display: flex;
+  height: 3.7rem;
+  width: 45rem;
+  z-index: 2;
+}
+#r12 {
+  left: 81.3rem;
+  top: 7.6rem;
   position: absolute;
   height: 30rem;
   width: 4rem;
