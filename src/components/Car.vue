c1ca2acd5c06f2d5e4894021c350b0016d345833
mengkai <363255751@qq.com>
2025-01-21 11:21:51 +0800

    1

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,17 +1,40 @@
 <template>
     <div class="car">
-     19
+        <el-text class="left" size="large"> {{ number }}</el-text>
+
     </div>
 </template>
 <script setup>
+defineProps({
 
+    number:
+    {
+        type: Number,
+        required: true,
+    }
+})
 </script>
 <style scoped>
-.car{
-    width: 32px;
-    height: 58px;
+.car {
+    width: 3.5rem;
+    height: 1.3rem;
     background-color: gray;
     z-index: 3;
     border: 1px solid green;
+    margin-top: 0.2rem;
+    margin-bottom: 0.2rem;
+
+}
+
+.left {
+    font-size: 0.7rem;
+    color: rgb(218, 218, 218);
+    margin-left: 0.5rem;
+}
+
+.right {
+    font-size: 0.7rem;
+    color: rgb(218, 218, 218);
+    margin-right: 0.5rem;
 }
 </style>
--- src/components/CarPark.vue
+++ src/components/CarPark.vue
@@ -1,74 +1,68 @@
 <template>
   <div class="parking-lot-container" ref="container">
-    <!-- <img src="../assets/parking-lot.jpg" alt="停车场背景" class="background-image" /> -->
-
     <div id="carparkleft">
-      <Car2 :number="n" v-for=" n in 19" :key="n"></Car2>
-
+      <Car :number="n" v-for=" n in 19" :key="n"></Car>
     </div>
 
-    <div id="carparkcenter">
-      <Car2 :number="n" v-for=" n in 19" :key="n"></Car2>
+   <div id="carparkcenter">
+      <Car :number="n" v-for=" n in 19" :key="n"></Car>
     </div>
     <div id="carparkcenter2">
-      <Car2 :number="n" v-for=" n in 19" :key="n"></Car2>
-    </div>
+      <Car :number="n" v-for=" n in 19" :key="n"></Car>
+    </div> 
 
   </div>
 </template>
 
 <script setup>
 import Car from '../components/Car.vue';
-import Car2 from '../components/Car2.vue';
-
 </script>
 
 <style lang="scss" scoped>
-$parking-lot: '~@/assets/parking-lot.jpg';
+$parking-lot: '../assets/parking-lot.jpg';
 #carparkcenter {
-  left: 230px;
-  top: 80px;
+  left: 8.2rem;;
+  top: 4rem;
   position: absolute;
-  height: 600px;
-  width: 100px;
-  border: 1px solid red;
+  height: 29rem;
+  width: 4rem;
+  // border: 1px solid red;
   z-index: 2;
 }
 
 #carparkcenter2 {
-  left: 280px;
-  top: 80px;
+  left: 11.8rem;;
+  top: 4rem;
   position: absolute;
-  height: 600px;
-  width: 100px;
-  border: 1px solid red;
+  height: 29rem;
+  width: 4rem;
+  // border: 1px solid red;
   z-index: 2;
 }
-
 
 #carparkleft {
-  left: 30px;
-  top: 80px;
+  left: 1.2rem;
+  top: 4rem;
   position: absolute;
-  height: 600px;
-  width: 100px;
-  border: 1px solid red;
+  height: 29rem;
+  width: 4rem;
+  // border: 1px solid red;
   z-index: 2;
 }
-
+// 1830  
+// 840
+// 45.75%;
 .parking-lot-container {
+  font-size: 4px;
   margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 1280px;
-  height: 0;
-  padding-top: 45.75%;
+  position: relative;
+  width: 85rem;
+  height: 39.4rem;
+  // padding-top: 45.75%;
   background-image: url($parking-lot);
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  border: 1px solid rgb(37, 145, 15);
 }
 
 </style>
