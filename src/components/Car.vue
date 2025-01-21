ae37b269f8287e669eaa8ac0e87ffbd2d7255560
mengkai <363255751@qq.com>
2025-01-21 15:02:16 +0800

    1

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,40 +1,89 @@
 <template>
-    <div class="car">
-        <el-text class="left" size="large"> {{ number }}</el-text>
+    <div :class="headerDirection" :style="{ backgroundColor :carbackgroundColor}">
+        <el-text :class="numberPosition" size="large"> 0{{ number }}</el-text>
 
     </div>
 </template>
 <script setup>
-defineProps({
+import { ref } from 'vue';
+const props = defineProps({
+    headerDirection:
+    {
+        type: String,
+        default: "horizontal" // horizontal | vertical 
 
+    },
+    numberPosition:
+    {
+        type: String,
+        default: "left" // left | right | center
+
+    },
     number:
     {
         type: Number,
-        required: true,
+        default: -1
+    }
+    ,
+    hasCar:
+    {
+        type: Boolean,
+        default: false
     }
 })
+let carbackgroundColor = ref("#909090");
+function setCarColor() {
+    if (props.hasCar) {
+        carbackgroundColor.value = "#49c298";
+    }
+}
+function init() {
+    setCarColor();
+}
+init()
 </script>
-<style scoped>
-.car {
+<style scoped lang="scss">
+.horizontal {
     width: 3.5rem;
     height: 1.3rem;
-    background-color: gray;
+    // background-color: gray;
     z-index: 3;
-    border: 1px solid green;
+    /* border: 1px solid green; */
     margin-top: 0.2rem;
     margin-bottom: 0.2rem;
+    position: relative;
 
+}
+
+.vertical {
+    height: 3.5rem;
+    width: 1.1rem;
+    // background-color: gray;
+    z-index: 3;
+    /* border: 1px solid green; */
+    margin-top: 0.2rem;
+    margin-bottom: 0.2rem;
+    position: relative;
 }
 
 .left {
     font-size: 0.7rem;
     color: rgb(218, 218, 218);
     margin-left: 0.5rem;
+
 }
 
 .right {
     font-size: 0.7rem;
     color: rgb(218, 218, 218);
-    margin-right: 0.5rem;
+    position: absolute;
+    right: 0.5rem;
+}
+
+.center {
+    font-size: 0.7rem;
+    color: rgb(218, 218, 218);
+    position: absolute;
+    right: 1rem;
 }
 </style>
--- src/components/CarPark.vue
+++ src/components/CarPark.vue
@@ -1,27 +1,77 @@
 <template>
   <div class="parking-lot-container" ref="container">
-    <div id="carparkleft">
-      <Car :number="n" v-for=" n in 19" :key="n"></Car>
-    </div>
+    <el-text id="label" size="large">洗车台</el-text>
 
-   <div id="carparkcenter">
-      <Car :number="n" v-for=" n in 19" :key="n"></Car>
+    <div id="carparkleft">
+      <Car :number="n" v-for=" n in 19" :key="n" number-position="right" :has-car="true"></Car>
+    </div>
+    <div class="overlay"></div>
+
+    <div id="carparkcenter">
+      <Car :number="n" v-for=" n in 18  " :key="n"></Car>
     </div>
     <div id="carparkcenter2">
-      <Car :number="n" v-for=" n in 19" :key="n"></Car>
-    </div> 
+      <Car :number="n" v-for=" n in 20" :key="n" number-position="right"></Car>>
+    </div>
+
+
+    <div id="carparkright">
+      <Car :number="n" v-for=" n in 18  " :key="n"></Car>
+    </div>
+    <div id="carparkright2">
+      <Car :number="n" v-for=" n in 20" :key="n" number-position="right"></Car>>
+    </div>
+
+    <div id="rightleft">
+      <Car :number="n" v-for=" n in 19" :key="n" number-position="right" :has-car="true"></Car>
+    </div>
 
   </div>
 </template>
 
 <script setup>
 import Car from '../components/Car.vue';
+
+let data = [
+  { number: 1, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
+  { number: 2, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
+  { number: 3, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
+  { number: 4, isStop: false, isLeave: false, isEmpty: true ,place:"膜结构车棚"},
+  { number: 5, isStop: false, isLeave: false, isEmpty: true ,place:"地下车库"},
+  { number: 6, isStop: false, isLeave: false, isEmpty: true ,place:"地下车库"}
+]
+let lLeft=ref([]);
+let lCenter=ref([]);
+let lRight=ref([]);
+
+function allotData()
+{
+  lLeft.value=data.number.filter()
+
+}
+function init(){
+ 
+
+}
+ init();
 </script>
 
 <style lang="scss" scoped>
 $parking-lot: '../assets/parking-lot.jpg';
+
+
+
+#label {
+  font-size: 0.8rem;
+  color: rgb(7, 88, 88);
+  left: 21.5rem;
+  top: 0.93rem;
+  position: absolute;
+  z-index: 2;
+}
+
 #carparkcenter {
-  left: 8.2rem;;
+  left: 8rem;
   top: 4rem;
   position: absolute;
   height: 29rem;
@@ -31,7 +81,27 @@ $parking-lot: '../assets/parking-lot.jpg';
 }
 
 #carparkcenter2 {
-  left: 11.8rem;;
+  left: 12rem;
+  top: 4rem;
+  position: absolute;
+  height: 29rem;
+  width: 4rem;
+  // border: 1px solid red;
+  z-index: 2;
+}
+
+#carparkright {
+  left: 18.8rem;
+  top: 4rem;
+  position: absolute;
+  height: 29rem;
+  width: 4rem;
+  // border: 1px solid red;
+  z-index: 2;
+}
+
+#carparkright2 {
+  left: 22.8rem;
   top: 4rem;
   position: absolute;
   height: 29rem;
@@ -44,11 +114,44 @@ $parking-lot: '../assets/parking-lot.jpg';
   left: 1.2rem;
   top: 4rem;
   position: absolute;
-  height: 29rem;
+  height: 30rem;
   width: 4rem;
   // border: 1px solid red;
   z-index: 2;
 }
+
+.overlay {
+  left: 0.9rem;
+  top: 3.7rem;
+  position: absolute;
+  height: 29.3rem;
+  width: 1.5rem;
+  z-index: 2;
+  background-color: #3bb1b1;
+  // background-color: #4b7373;
+  opacity: 0.7;
+}
+
+.overlay::before {
+  // content: '2';
+  position: absolute;
+  top: -10px;
+  /* 调整边框位置 */
+  left: -10px;
+  right: -10px;
+  bottom: -10px;
+  border: 2px dashed transparent;
+  /* 透明边框 */
+  // background: linear-gradient(45deg, transparent 25%, #fff 25%, #fff 50%, transparent 50%, transparent 75%, #fff 75%, #fff 100%);
+  background-size: 40px 40px;
+  /* 花纹大小 */
+  z-index: -1;
+  /* 确保在覆盖层背景之下 */
+  pointer-events: none;
+  /* 确保不影响覆盖层的交互 */
+}
+
+
 // 1830  
 // 840
 // 45.75%;
@@ -64,5 +167,4 @@ $parking-lot: '../assets/parking-lot.jpg';
   background-position: center;
   background-repeat: no-repeat;
 }
-
 </style>
